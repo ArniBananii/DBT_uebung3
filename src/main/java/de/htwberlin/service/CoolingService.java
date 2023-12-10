7a8ff42042410150965700c86ae5c44d48002ed8
jakobmertenhtw <147500519+jakobmertenhtw@users.noreply.github.com>
2023-12-10 17:07:40 +0100

    Update CoolingService.java

--- src/main/java/de/htwberlin/service/CoolingService.java
+++ src/main/java/de/htwberlin/service/CoolingService.java
@@ -16,12 +16,12 @@ import org.slf4j.LoggerFactory;
 import de.htwberlin.exceptions.DataException;
 
 public class CoolingService implements ICoolingService {
-private SampleDao sampleDao;
-private TrayDao trayDao;
-//private PlaceDao placeDao;
-private Sample sample;
-private Tray tray;
-private List<Tray> trayList;
+  private SampleDao sampleDao;
+  private TrayDao trayDao;
+  //private PlaceDao placeDao;
+  private Sample sample;
+  private Tray tray;
+  private List<Tray> trayList;
   // Test commit
 
   private static final Logger L = LoggerFactory.getLogger(CoolingService.class);
@@ -44,35 +44,51 @@ private List<Tray> trayList;
   public void transferSample(Integer sampleId, Integer diameterInCM) {
     L.info("transferSample: sampleId: " + sampleId + ", diameterInCM: " + diameterInCM);
 
-
-    //my check for now
     trayDao = new TrayDao();
     sampleDao = new SampleDao();
-//    placeDao = new PlaceDao();
 
-//    placeDao.setConnection(useConnection());
     sampleDao.setConnection(useConnection());
     trayDao.setConnection(useConnection());
 
+    // Initialisiert das Sample Objekt - wenn das Sample nicht existiert, dann wird eine CoolingSystemException geworfen
     sample = sampleDao.findBy(sampleId);
     if(sample == null){
       throw new CoolingSystemException("Sample with id " + sampleId + " does not exist");
     }
+
+    // Diese TrayList enthält alle Trays, die noch freie Plätze haben und der Diameter passt
     trayList = trayDao.findByDiameterAndCapacity(diameterInCM, sample);
+    L.info("Traylist wurde erstellt und das Logging für die TrayLists beginnen.");
+    for (Tray tray : trayList) {
+      L.info("Tray mit der ID: " +  tray.getTrayID() + "wurde zu der Tray List hinzugefügt");
+    }
     if(trayList == null || trayList.isEmpty()){
+      L.info("Kein Tray mit dem Durchmesser " + diameterInCM + " gefunden");
       throw new CoolingSystemException("Tray with diameter " + diameterInCM + " does not exist");
     }
 
-//    trayList = trayDao.findTraysWithFittingExperation(sample,trayList);
-    trayList.forEach((tray1 -> L.info(tray1.getTrayID().toString())));
+    // Get final Tray
+
+    Tray finalTray = trayDao.findFinalTray(sample,trayList);
+    if (finalTray.getSetExpirationDateManually()) {
+      L.info("Das ExpirationDate wird jetzt in die Datenbank geschrieben gesetzt");
+      trayDao.updateExpirationDate(finalTray);
+    }
+    L.info("Es wurde ein final Tray gefunden mit der ID: " + finalTray.getTrayID());
+    if(finalTray == null){
+      L.info("Kein Final Tray gefunden");
+      throw new CoolingSystemException("No Tray found");
+    }
+
+    trayDao.findPlace(finalTray, sample);
+
+
 
 
   }
 
 
 }
-
-
 
 
 
